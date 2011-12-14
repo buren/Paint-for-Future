722b6a3c6f485917aa94f231b19ff57112d92a40
Jacob Burenstam <burenstam@gmail.com>
2011-12-14 22:03:47 +0100

    update of sponsor page

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,4 @@
 class Video < ActiveRecord::Base
-  attr_accessible :title, :vimeo, :description
+  attr_accessible :title, :description
   validates :title, :presence => true, :length => { :minimum => 3 }
-  validates :vimeo, :presence => true, :length => { :minimum => 30 }
 end
