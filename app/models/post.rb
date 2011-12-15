f459fe93663067e53114286ba011f6fd86dc006c
Jacob Burenstam <burenstam@gmail.com>
2011-12-15 03:56:01 +0100

    removed title validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :image, :title, :name, :content, :remote_image_url, :published_at
   mount_uploader :image, PostImageUploader
-  validates :name, :presence => true, :length => { :minimum => 2 }
-  validates :title, :presence => true, :length => { :minimum => 3 }
+  validates :name, :presence => true
 end
 
