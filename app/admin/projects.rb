ddafcf02a85b576cf4edbe041e328d215f0a5c7c
Jacob Burenstam <burenstam@gmail.com>
2012-02-23 00:13:04 +0100

    updated admin interface so no link to delete exists on project index page

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Project do
-  
+  	index do
+   		column "Title" do |goal|
+      		link_to goal.title, admin_goal_path(goal)
+    	end
+   		column "Content" do |goal|
+    		link_to goal.content, admin_goal_path(goal)
+   		end
+  	end
 end
