f28f403f2a7ca343225b25622a4caad98d49e1f6
Jacob <burenstam@gmail.com>
2012-02-24 01:23:32 +0000

    admin inteface fix for project index page

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register Project do
   	index do
-   		column "Title" do |goal|
+   		column "Title" do |project|
       		link_to project.title, admin_project_path(project)
     	end
-   		column "Content" do |goal|
+   		column "Content" do |project|
     		link_to project.content, admin_project_path(project)
    		end
   	end
