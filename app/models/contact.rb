39d0711f3fd16387c627aec5770afd0b9d1de5a2
Jacob Burenstam <burenstam@gmail.com>
2012-08-22 21:45:18 +0200

    added webite column for contacts to attr accs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  attr_accessible :name, :email, :phone_number, :description, :job_title, :image
+  attr_accessible :name, :email, :phone_number, :description, :job_title, :image, :website
   mount_uploader :image, PostImageUploader
   validates :name, :presence => true
   validates :email, :presence => true
