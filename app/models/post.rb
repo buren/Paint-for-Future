b46cbf3dca0a620ff48cb2de3b6b6f3fb3f2ee67
Jacob Burenstam <burenstam@gmail.com>
2012-08-24 16:37:39 +0200

    change font to oswald

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :image, :title, :name, :content, :remote_image_url, :published_at
   mount_uploader :image, PostImageUploader
-  validates :name, :title, :presence => true
+  validates :name, :title, :published_at, :content, :presence => true
 end
 
