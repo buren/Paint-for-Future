10364030a8f89981401df73c09b35f2d7f6a900a
Jacob Burenstam <burenstam@gmail.com>
2012-08-24 17:41:48 +0200

    added published at column to the posts new form

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register Post do
     f.inputs "Post", :multipart => true do
       f.input :name, :as => :select, :collection => ["Fredrik Beck", "Susanne Holmsater", "Lucas Lennig", "Daniel Leksell"], :hint => "Your name"
       f.input :title
+      f.input :published_at
       f.input :content, :hint => "You can use HTML, use <br> tag for new line, <h3> for header and <b> for bold"
       f.input :remote_image_url, :as => :url, :hint => "Provide URL to jpg, giff or png image"
       f.input :image, :as => :file, :hint => "Select any jpg, giff or png image"
