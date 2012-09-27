1252115bab588caee8d27b85fbf164500919d102
Jacob Burenstam <burenstam@gmail.com>
2012-09-27 11:51:38 +0200

    small fixes gallery index

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,8 +1,8 @@
 class GalleriesController < ApplicationController
 	
 	def index
-		@galleries = Gallery.all
-    @messages = Message.order("created_at desc")
+		@galleries = Gallery.order("created_at desc")
+    	@messages = Message.order("created_at desc")
 
 		respond_to do |format|
 			format.html # index.html.erb
