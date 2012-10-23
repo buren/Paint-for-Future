9aca8f07366d4b89546c627268671c5686577cc0
Jacob Burenstam <burenstam@gmail.com>
2012-10-23 19:27:12 +0200

    Added the  name to the url for each contact link

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,8 @@ class Contact < ActiveRecord::Base
   mount_uploader :image, PostImageUploader
   validates :name, :presence => true
   validates :email, :presence => true
+
+  def to_param
+  	"#{id} #{name}".parameterize
+  end
 end
