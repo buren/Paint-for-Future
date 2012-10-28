ed9ca5eefe3493bfc2a7e3056280e23ec3f7977f
Jacob Burenstam <burenstam@gmail.com>
2012-10-28 16:43:45 +0100

    fixed title for fancybox images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	        closeEffect	: 'elastic',
             helpers: {
                     title : {
-                            type : 'float'
+                            type : 'inside'
                     }
             }
 	 });
