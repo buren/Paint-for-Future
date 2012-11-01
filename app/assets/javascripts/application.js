d9efa67e484a0053cf7f0a2d9ed82cae28984b4a
Jacob Burenstam <burenstam@gmail.com>
2012-11-02 00:56:48 +0100

    added google translator to the meta tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,20 @@
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
 //
 //= require jquery
 //= require active_admin
 
 
 $(document).ready(function() {
-    $("a.fancybox").fancybox({
-      openEffect	: 'elastic',
-	        closeEffect	: 'elastic',
-            helpers: {
-                    title : {
-                            type : 'inside'
-                    }
-            }
-	 });
+
+    
+    $('.fancybox').fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+        helpers: {
+          title : {
+            type : 'inside'
+          }
+        }
+    });
    
   $("a.external-links").click(function() {
     link_host = this.href.split("/")[2];
